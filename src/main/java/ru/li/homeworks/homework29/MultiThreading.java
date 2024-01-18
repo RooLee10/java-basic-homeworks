d07d31a9b4db4194c417b8664a6bb50a8a1640c3
Vadim Li <Vadim@DESKTOP-EM6HT12.(none)>
2024-01-18 22:59:36 +0300

    HW 29 done

---
+++ src/main/java/ru/li/homeworks/homework29/MultiThreading.java
@@ -0,0 +1,62 @@
+package ru.li.homeworks.homework29;
+
+public class MultiThreading {
+    private final Object mon;
+    private String letter;
+
+    public MultiThreading() {
+        this.mon = new Object();
+        this.letter = "";
+    }
+
+    public void printA() {
+        synchronized (mon) {
+            for (int i = 0; i < 5; i++) {
+                while (!(letter.isEmpty() | letter.equals("C"))) {
+                    try {
+                        mon.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                letter = "A";
+                System.out.print(letter);
+                mon.notifyAll();
+            }
+        }
+    }
+
+    public void printB() {
+        synchronized (mon) {
+            for (int i = 0; i < 5; i++) {
+                while (!letter.equals("A")) {
+                    try {
+                        mon.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                letter = "B";
+                System.out.print(letter);
+                mon.notifyAll();
+            }
+        }
+    }
+
+    public void printC() {
+        synchronized (mon) {
+            for (int i = 0; i < 5; i++) {
+                while (!letter.equals("B")) {
+                    try {
+                        mon.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                letter = "C";
+                System.out.print(letter);
+                mon.notifyAll();
+            }
+        }
+    }
+}
